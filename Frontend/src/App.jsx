7ab4bab38396abcbd582c567bf44691b4cdb4450
Fu Jun Pan <fujunp9@gmail.com>
2025-02-14 15:32:20 -0500

    Added routes and restricted non mobile users

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./pages/Home/Home.jsx";
+import { UnsupportedDevice } from "./pages/UnsupportedDevice/UnsupportedDevice.jsx";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function userIsMobile() {
+  return /Mobi|Android|iPhone|iPad|iPod/i.test(window.navigator.userAgent);
 }
 
-export default App
+export default function App() {
+  const isMobile = userIsMobile();
+  return (
+    <>
+      <Routes>
+      <Route path="/" element={isMobile ? <Home /> : <Navigate to="/UnsupportedDevice" />} />
+      <Route path="/UnsupportedDevice" element={isMobile ? <Navigate to="/" /> : <UnsupportedDevice />} />
+      </Routes>
+    </>
+  );
+};
