f42a1ea6110011e5370897871031bbab512c3a30
Sadia <Sadia.zalmay@gmail.com>
2025-02-16 02:48:08 -0500

    Updated the requirement.txt for backend and name of website

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'EduOCR',
+  title: 'Penfluence',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
