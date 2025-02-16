5d414b853bdce1940ffbb313d4368bfb718a43ac
FardinAhmed3 <ahmedfardin077@gmail.com>
2025-02-16 02:07:21 -0500

    (FJP) Modified Navbar

--- Frontend/components/Navbar.tsx
+++ Frontend/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from "next/link"
 import { useState } from "react"
+import Image from "next/image"
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
@@ -10,14 +11,18 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex justify-between w-full">
-            <div className="flex-shrink-0 flex items-center">
-              <Link 
-                href="/"
-                className="text-2xl font-bold text-white"
-              >
-                EduOCR
+            <div className="flex items-center mt-5"> {/* Added margin-top to lower the logo */}
+            <Link href="/">
+              <Image
+                src="/logo.png"
+                alt="Logo"
+                width={200}
+                height={200}
+                className="cursor-pointer" // Adds a pointer cursor on hover
+              />
               </Link>
             </div>
+
             <div className="hidden sm:flex sm:items-center sm:space-x-8">
               {/* <Link
                 href="/"
@@ -87,4 +92,3 @@ export default function Navbar() {
     </nav>
   )
 }
-
