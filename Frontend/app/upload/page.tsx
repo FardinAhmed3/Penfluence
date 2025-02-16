320509ce88c16519e72b5d5fd47b55de6dc26b55
FardinAhmed3 <ahmedfardin077@gmail.com>
2025-02-16 03:37:48 -0500

    Fixed API fetch URL

--- Frontend/app/upload/page.tsx
+++ Frontend/app/upload/page.tsx
@@ -28,7 +28,7 @@ export default function UploadPage() {
     formData.append("file", file);
 
     try {
-      const response = await fetch("http://192.168.0.2:8000/upload/", {
+      const response = await fetch("http://127.0.0.1:8000/upload/", {
         method: "POST",
         body: formData,
       });
